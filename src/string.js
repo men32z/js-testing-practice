afbe679433219e1ca2ccf496589731ff500ad97c
Luis Preza <preza.luis@gmail.com>
2020-02-13 10:30:28 -0600

    initial config

---
+++ src/string.js
@@ -0,0 +1,5 @@
+function capitalize(){
+  return "Hello";
+}
+
+module.exports = capitalize;
---
+++ test/string.test.js
@@ -0,0 +1,5 @@
+const capitalize = require('../src/string');
+
+test('test', () => {
+  expect(capitalize()).toBe("Hello");
+});
